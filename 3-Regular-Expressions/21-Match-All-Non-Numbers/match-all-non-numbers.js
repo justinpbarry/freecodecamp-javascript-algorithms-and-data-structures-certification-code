aa330c4188fca1103a7466430d8f41dc311ba6fc
Justin <websites@linuxwebdevelopment.com>
2019-06-10 00:25:50 -0500

    Some more JavaScript solutions. Still need to work on 22.

--- 3-Regular-Expressions/21-Match-All-Non-Numbers/match-all-non-numbers.js
+++ 3-Regular-Expressions/21-Match-All-Non-Numbers/match-all-non-numbers.js
@@ -4,4 +4,7 @@
     You are free to use this code as you like. Please just give me credit.
 */
 
-
+let numString = "Your sandwich will be $5.00";
+let noNumRegex = /\D/g;
+let result = numString.match(noNumRegex).length;
+console.log(result);
--- 3-Regular-Expressions/22-Restrict-Possible-Usernames/restrict-possible-usernames.js
+++ 3-Regular-Expressions/22-Restrict-Possible-Usernames/restrict-possible-usernames.js
@@ -4,4 +4,23 @@
     You are free to use this code as you like. Please just give me credit.
 */
 
+// let username = "JackOfAllTrades";
 
+// NEED TO FINISH !!! For some reason it isn't accepting my solution
+let username = "JACK";
+let userCheck = /[a-zA-Z][a-zA-Z][a-zA-Z]*\d*$/g;
+let result = userCheck.test(username);
+console.log(result);
+
+
+let username = "JACK";
+let userCheck = /[a-zA-Z][a-zA-Z]+\d*$/g;
+let result = userCheck.test(username);
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-zA-Z][a-zA-Z]+\d*$/g;
+let result = userCheck.test(username);
+
+let username = "RegexGuru";
+let userCheck = /^[a-zA-Z][a-zA-Z]+\d*$/g;
+let result = userCheck.test(username);
