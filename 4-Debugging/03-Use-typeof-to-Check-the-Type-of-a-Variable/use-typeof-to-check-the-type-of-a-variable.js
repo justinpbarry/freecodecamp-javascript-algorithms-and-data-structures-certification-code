45258277c7ea4ba4bf52e91866cfb3b52ebe7a2c
Justin <websites@linuxwebdevelopment.com>
2019-06-19 01:05:53 -0500

    adding more debugging solutions

--- 4-Debugging/03-Use-typeof-to-Check-the-Type-of-a-Variable/use-typeof-to-check-the-type-of-a-variable.js
+++ 4-Debugging/03-Use-typeof-to-Check-the-Type-of-a-Variable/use-typeof-to-check-the-type-of-a-variable.js
@@ -4,4 +4,8 @@
     You are free to use this code as you like. Please just give me credit.
 */
 
-
+let seven = 7;
+let three = "3";
+console.log(seven + three);
+console.log(typeof seven);
+console.log(typeof three);
--- 4-Debugging/04-Catch-Misspelled-Variable-and-Function-Names/catch-misspelled-variable-and-function-names.js
+++ 4-Debugging/04-Catch-Misspelled-Variable-and-Function-Names/catch-misspelled-variable-and-function-names.js
@@ -4,4 +4,16 @@
     You are free to use this code as you like. Please just give me credit.
 */
 
+// Fix the two spelling errors in the code so the netWorkingCapital calculation works.
 
+// original
+// let receivables = 10;
+// let payables = 8;
+// let netWorkingCapital = recievables - payable;
+// console.log(`Net working capital is: ${netWorkingCapital}`);
+
+// fixed
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
