1b0037ced8946e3cc1f19c44b7d53d457d2220cd
justin <justin@debian>
2019-07-04 03:07:46 -0500

    more functional programming section solutions

--- 8-Functional-Programming/04-Avoid-Mutations-and-Side-Effects-Using-Functional-Programming/avoid-mutations-and-side-effects-using-functional-programming.js
+++ 8-Functional-Programming/04-Avoid-Mutations-and-Side-Effects-Using-Functional-Programming/avoid-mutations-and-side-effects-using-functional-programming.js
@@ -4,4 +4,18 @@
     You are free to use this code as you like. Please just give me credit.
 */
 
+// Fill in the code for the function incrementer so it returns the value of the
+// global variable fixedValue increased by one.
 
+// the global variable
+var fixedValue = 4;
+
+function incrementer () {
+  // Add your code below this line
+
+  return fixedValue + 1;
+  // Add your code above this line
+}
+
+var newValue = incrementer(); // Should equal 5
+console.log(fixedValue); // Should print 4
--- 8-Functional-Programming/05-Pass-Arguments-to-Avoid-External-Dependence-in-a-Function/pass-arguments-to-avoid-external-dependence-in-a-function.js
+++ 8-Functional-Programming/05-Pass-Arguments-to-Avoid-External-Dependence-in-a-Function/pass-arguments-to-avoid-external-dependence-in-a-function.js
@@ -4,4 +4,16 @@
     You are free to use this code as you like. Please just give me credit.
 */
 
+// Write the incrementer function so it takes an argument, and then increases the value by one.
 
+// the global variable
+var fixedValue = 4;
+
+// Add your code below this line
+function incrementer () {
+
+  // Add your code above this line
+}
+
+var newValue = incrementer(fixedValue); // Should equal 5
+console.log(fixedValue); // Should print 4
