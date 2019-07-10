2c87d949959bdefeb972b2323b56bf894870cea0
justin <justin@debian>
2019-07-10 03:30:26 -0500

    adding solutions to Functional Programming section

--- 8-Functional-Programming/10-Implement-the-filter-Method-on-a-Prototype/implement-the-filter-method-on-a-prototype.js
+++ 8-Functional-Programming/10-Implement-the-filter-Method-on-a-Prototype/implement-the-filter-method-on-a-prototype.js
@@ -4,4 +4,28 @@
     You are free to use this code as you like. Please just give me credit.
 */
 
+// Write your own Array.prototype.myFilter(), which should behave exactly like
+// Array.prototype.filter(). You may use a for loop or the Array.prototype.forEach() method.
 
+
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback){
+  var newArray = [];
+  // Add your code below this line
+
+  this.forEach(function(el) {
+      if(callback(el) == true) {
+          newArray.push(el);
+      }
+  })
+
+  // Add your code above this line
+  return newArray;
+
+};
+
+var new_s = s.myFilter(function(item){
+  return item % 2 === 1;
+});
