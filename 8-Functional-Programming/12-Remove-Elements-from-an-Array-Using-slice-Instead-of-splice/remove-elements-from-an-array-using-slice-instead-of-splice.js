d0b453f70aeb6c42bd4406c74a0c6d9fb1d6ed92
justin <justin@debian>
2019-07-12 00:25:34 -0500

    adding more solutions to Functional Programming section

--- 8-Functional-Programming/12-Remove-Elements-from-an-Array-Using-slice-Instead-of-splice/remove-elements-from-an-array-using-slice-instead-of-splice.js
+++ 8-Functional-Programming/12-Remove-Elements-from-an-Array-Using-slice-Instead-of-splice/remove-elements-from-an-array-using-slice-instead-of-splice.js
@@ -4,4 +4,16 @@
     You are free to use this code as you like. Please just give me credit.
 */
 
+// Rewrite the function nonMutatingSplice by using slice instead of splice. It
+// should limit the provided cities array to a length of 3, and return a new array with
+// only the first three items.
 
+function nonMutatingSplice(cities) {
+  // Add your code below this line
+  // return cities.splice(3);
+  return cities.slice(0,3);
+
+  // Add your code above this line
+}
+var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities);
